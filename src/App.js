80a5eae0d2c6fef65f08796045c3af07a4f542e1
lahasya <lahasya.lnu001@umb.edu>
2023-02-21 10:30:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Viewer from './components/Viewer';
+import Drawer from './components/Drawer';
 
 function App() {
+  const someUrl = "https://niivue.github.io/niivue-demo-images/visiblehuman.nii.gz";
+  //const someUrl = "https://niivue.github.io/niivue-demo-images/mni152.nii.gz";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Drawer url={someUrl}/>
+   // <Viewer imageUrl={someUrl}> </Viewer>
+
   );
 }
 
